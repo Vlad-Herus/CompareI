ef8f59d9ce411ce0e6060ba57137956446d1f67c
Vladimir <Vladimir@CEntralInnovation.com>
2018-05-09 01:10:44 +1000

    Initial

---
+++ CompareI/FileContext.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace Test3
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>.
+    [ProvideAutoLoad("f1536ef8-92ec-443c-9ed7-fdadf150da82")]
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [Guid(SolutionMenuContextPackage.PackageGuidString)]
+    internal sealed class FileContext 
+    {
+        private System.Collections.Specialized.StringCollection _exts;
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 256;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("eee616b9-f58d-40d6-ac19-4a3ee4334503");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileContext"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private FileContext(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+            _exts =  GlobalSettings.Default.EXTENS_FILTER;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                 OleMenuCommand menuItem = new OleMenuCommand(this.MenuItemCallback, menuCommandID);
+                 menuItem.BeforeQueryStatus += MenuItem_BeforeQueryStatus;
+                commandService.AddCommand(menuItem);
+            }           
+        }
+
+        private void MenuItem_BeforeQueryStatus(object sender, EventArgs e)
+        {
+            OleMenuCommand menuCommand = sender as OleMenuCommand;
+            if (menuCommand != null)
+            {
+                string itemFullPath = SelectedItemUtil.GetFullFilePath(ServiceProvider);
+                string itemExt = itemFullPath?.Substring(itemFullPath.LastIndexOf('.') + 1);
+
+                menuCommand.Visible = _exts.Contains(itemExt);
+            }            
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static FileContext Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private System.IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new FileContext(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            //TODO: Put your code here
+
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "FileContext";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.ServiceProvider,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
---
+++ CompareI/SelectedItemUtil.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Test3
+{
+    class SelectedItemUtil
+    {
+
+        public static string GetFullFilePath(IServiceProvider serviceProvider)
+        {
+            var selectionMonitor = serviceProvider.GetService(typeof(SVsShellMonitorSelection)) as IVsMonitorSelection;
+            if (selectionMonitor == null)
+            {
+                Debug.Fail("Failed to get SVsShellMonitorSelection service.");
+
+                return null;
+            }
+
+            int hr = selectionMonitor.GetCurrentSelection(out IntPtr hierarchyPtr, out uint itemid, out IVsMultiItemSelect multiSelect, out IntPtr containerPtr);
+            if (IntPtr.Zero != containerPtr)
+            {
+                Marshal.Release(containerPtr);
+                containerPtr = IntPtr.Zero;
+            }
+            Debug.Assert(hr == VSConstants.S_OK, "GetCurrentSelection failed.");
+
+            if (itemid == (uint)VSConstants.VSITEMID.Selection)
+            {
+                //multiple selection
+                return null;
+            }
+
+            if (hierarchyPtr != IntPtr.Zero)
+            {
+                IVsProject project = Marshal.GetUniqueObjectForIUnknown(hierarchyPtr) as IVsProject;
+                string itemFullPath = null;
+                project?.GetMkDocument(itemid, out itemFullPath);
+
+                return itemFullPath;
+
+            }
+
+
+            return null;
+        }
+    }
+}
