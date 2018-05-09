1ae896c4fc89e436c4fb1a1f640da641021c4574
Vladimir <Vladimir@CEntralInnovation.com>
2018-05-10 01:12:56 +1000

    Multiselect support for file command

--- CompareI/SelectedItemUtil.cs
+++ CompareI/SelectedItemUtil.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell.Interop;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace Test3
@@ -9,14 +11,14 @@ namespace Test3
     class SelectedItemUtil
     {
 
-        public static string GetFullFilePath(IServiceProvider serviceProvider)
+        public static IEnumerable<string> GetFullFilePath(IServiceProvider serviceProvider)
         {
             var selectionMonitor = serviceProvider.GetService(typeof(SVsShellMonitorSelection)) as IVsMonitorSelection;
             if (selectionMonitor == null)
             {
                 Debug.Fail("Failed to get SVsShellMonitorSelection service.");
 
-                return null;
+                return Enumerable.Empty<string>();
             }
 
             int hr = selectionMonitor.GetCurrentSelection(out IntPtr hierarchyPtr, out uint itemid, out IVsMultiItemSelect multiSelect, out IntPtr containerPtr);
@@ -29,8 +31,24 @@ namespace Test3
 
             if (itemid == (uint)VSConstants.VSITEMID.Selection)
             {
-                //multiple selection
-                return null;
+                multiSelect.GetSelectionInfo(out uint count, out int somethingElse);
+                if (count > 0)
+                {
+                    VSITEMSELECTION[] selectedItems = new VSITEMSELECTION[count];
+                    var sel = multiSelect.GetSelectedItems((uint)__VSGSIFLAGS.GSI_fOmitHierPtrs, count, selectedItems);
+
+                    foreach (var selectedItem in selectedItems)
+                    {
+
+                        int n = 22 + 12;
+                    }
+                    //multiple selection
+                    return return Enumerable.Empty<string>();
+                }
+            }
+            else
+            {
+
             }
 
             if (hierarchyPtr != IntPtr.Zero)
@@ -44,7 +62,16 @@ namespace Test3
             }
 
 
-            return null;
+            return return Enumerable.Empty<string>();
+        }
+
+        static string GetItemPath(IntPtr hirarchryPtr, uint temId)
+        {
+            IVsProject project = Marshal.GetUniqueObjectForIUnknown(hirarchryPtr) as IVsProject;
+            string itemFullPath = null;
+            project?.GetMkDocument(temId, out itemFullPath);
+
+            return itemFullPath;
         }
     }
 }
